1b12b0a03505904144d06fa58579643ba68bc3a4
name <varunbrahmane30@gmail.com>
2021-07-28 21:41:59 +0530

    [varun] added: validate Password rule 4 using exception handling

--- Pattern.cs
+++ Pattern.cs
@@ -11,7 +11,7 @@ namespace Day21_ExceptionHandlingUserRegistration
         public static string Regex_lName = "^[A-Z]{1}[A-Za-z]{1}[a-zA-Z]*$";
         public static string Regex_Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
         public static string Regex_MobileNo = "(0|91)?[ ][6-9][0-9]{9}";
-        public static string Regex_Password = "^*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$";
+        public static string Regex_Password = "^*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
 
         public bool validateFirstName(string firstName)
         {
