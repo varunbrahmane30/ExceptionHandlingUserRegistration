d27c8916e482c7edd116e12832e5fc50d7434adf
name <varunbrahmane30@gmail.com>
2021-07-28 20:39:11 +0530

    [varun] added: register First name of User

---
+++ RegisterUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day21_ExceptionHandlingUserRegistration
+{
+    public class RegisterUser : Exception
+    {
+        public enum ExceptionsType
+        {
+            Invalid_MESSAGE,
+        }
+        public ExceptionsType type;
+        public RegisterUser(ExceptionsType Type, string message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
