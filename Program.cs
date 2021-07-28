7203708ec0f2c04e723ec2adf1f8ed2a9f28b669
name <varunbrahmane30@gmail.com>
2021-07-28 20:12:04 +0530

    [varun] added: main program file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day21_ExceptionHandlingUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" User Registration Problem Using Exception Handling. ");
+            Console.ReadKey();
+        }
+    }
+}
